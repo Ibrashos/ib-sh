452f4a69e780ca406e333383602cf7bed54755a5
Ibrashos <Ibrashos@gmail.com>
2022-11-21 17:45:37 +0300

    8.5 8.5

--- Python Lessons/Глава 8 Функции/8.4.py
+++ Python Lessons/Глава 8 Функции/8.4.py
@@ -1,3 +1,4 @@
 def make_shirt(size='L', printer="I love python"):
     print(f"T-shirt with size {size} and print {printer}")
 make_shirt()
+make_shirt('XL', 'Top Father')
---
+++ Python Lessons/Глава 8 Функции/8.5.py
@@ -0,0 +1,5 @@
+def describe_city(city='reykjavik', country='iceland'):
+    print(f"{city.title()} is in {country.title()}")
+describe_city()
+describe_city(city='manjaro')
+describe_city('moscow', 'russia')
