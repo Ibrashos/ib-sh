16e678d850ce3884d19e8a41897245da8b412544
Ibrashos <Ibrashos@gmail.com>
2022-11-24 17:18:11 +0300

    reee

--- Python Lessons/wwee/ipd.py
+++ Python Lessons/wwee/ipd.py
@@ -1,3 +1,3 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto(b'My message', ('10.10.0.16', 8888))
+s.sendto(b'input()', ('10.10.0.16', 8888))
--- Python Lessons/wwee/sipd.py
+++ Python Lessons/wwee/sipd.py
@@ -1,6 +1,7 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('10.10.0.16', 8888))
-result = s.recv(1024)
-print('Message:', result.decode())
-s.close()
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('10.10.0.16', 8888))
+    result = s.recv(1024)
+    print('Message:', result.decode())
+    s.close()
