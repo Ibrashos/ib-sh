dd033f8d7cfa8f714307c2a2fc923fe7c5fd2dff
Ibrashos <Ibrashos@gmail.com>
2022-11-24 13:48:36 +0300

    ip sending and upgrade 8.8.py

---
+++ Python Lessons/wwee/sipd.py
@@ -0,0 +1,6 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('10.10.0.16', 8888))
+result = s.recv(1024)
+print('Message:', result.decode())
+s.close()
--- Python Lessons/Глава 8 Функции/8.8.py
+++ Python Lessons/Глава 8 Функции/8.8.py
@@ -1,8 +1,24 @@
-def make_album(author, album):
-	return {'Author': author, 'Album': album}
+# def make_album(author, album):
+# 	return {'Author': author, 'Album': album}
 
+# while True:
+# 	au_name = input("Введите 'q' для выхода\nВведите имя автора: ")
+# 	if au_name == 'q':
+# 		break
+# 	print(f"{make_album(au_name.title(), input('Введите название альбома: ').title())}")
+
+# 	
+
+def make_album(author, album):
+	if album == 'q':
+		raise SystemExit(1)
+	full_album = {'Author': author.title(), 'Album': album.title()}
+	return full_album
 while True:
-	au_name = input("Введите 'q' для выхода\nВведите имя автора: ")
-	if au_name == 'q':
+	author_name = input("Введите 'q' для выхода\nВведите имя автора: ")
+	if author_name == 'q':
 		break
-	print(f"{make_album(au_name.title(), input('Введите название альбома: ').title())}")
+	print(make_album(author_name, input('Введите название альбома: ')))
+
+
+
